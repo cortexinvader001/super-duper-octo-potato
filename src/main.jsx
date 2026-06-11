e8f6259afb2830aa762ec7ea2665176838168fb1
Kolawole Suleiman <kolawolesuleiman716@gmail.com>
2026-06-11 20:04:42 +0100

    Switch from BrowserRouter to HashRouter for hash-based routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
 import './styles/global.css'
@@ -8,9 +8,9 @@ import './styles/global.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </HelmetProvider>
   </React.StrictMode>
 )
